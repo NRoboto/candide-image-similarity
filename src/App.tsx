582bb544761cfc05ef92165f42255dc61bc6e265
Mike <mnightingale@tutanota.com>
2021-01-26 20:30:32 +0000

    Add test image card

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Col,
+  Container,
+  CardSubtitle,
+} from "reactstrap";
+import "../node_modules/bootswatch/dist/pulse/bootstrap.min.css";
+import "./App.css";
+
+import fileDescriptions from "./data/fileDescriptions.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="d-flex flex-column flex-md-row">
+      <Col xs="12" md="6" className="order-md-1">
+        <Card>
+          <CardImg top width="100%" src="/images/1.jpg" alt="" />
+          <CardBody>
+            <CardTitle tag="h3">{fileDescriptions["1.jpg"].title}</CardTitle>
+            <hr />
+            <CardText>
+              {fileDescriptions["1.jpg"].description ?? "Description goes here"}
+            </CardText>
+            <hr />
+            <CardSubtitle className="text-muted">
+              {fileDescriptions["1.jpg"].tags.join(", ")}
+            </CardSubtitle>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col md="3"></Col>
+      <Col md="3" className="order-2"></Col>
+    </Container>
   );
 }
 
