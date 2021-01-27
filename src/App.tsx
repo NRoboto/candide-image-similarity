0000b39d9d0603165ff46e122df7a61592981ca2
Mike <mnightingale@tutanota.com>
2021-01-27 14:28:06 +0000

    Refactor display elements into single element

--- src/App.tsx
+++ src/App.tsx
@@ -16,43 +16,43 @@ import fileDescriptions from "./data/fileDescriptions.json";
 
 type FileName = keyof typeof fileDescriptions;
 
-// type FileDescription = typeof fileDescriptions[keyof typeof fileDescriptions];
-
 type DisplayProps = {
   imgName: FileName;
+  expanded?: boolean;
 };
 
-const SmallDisplay = ({ imgName }: DisplayProps) => (
-  <Card className="hover-card p-1 mb-2">
-    <CardImg
-      top
-      width="100%"
-      src={`/images/${imgName}`}
-      alt=""
-      className="display-small-img"
-    />
-    <CardBody>
-      <CardTitle tag="h3">{fileDescriptions[imgName].title}</CardTitle>
-    </CardBody>
-  </Card>
-);
+const Display = ({ imgName, expanded }: DisplayProps) => {
+  const description = fileDescriptions[imgName];
 
-const LargeDisplay = ({ imgName }: DisplayProps) => (
-  <Card className="hover-card p-1">
-    <CardImg top width="100%" src="/images/1.jpg" alt="" />
-    <CardBody>
-      <CardTitle tag="h3">{fileDescriptions["1.jpg"].title}</CardTitle>
-      <hr />
-      <CardText>
-        {fileDescriptions["1.jpg"].description ?? "Description goes here"}
-      </CardText>
-      <hr />
-      <CardSubtitle className="text-muted">
-        Tags: {fileDescriptions["1.jpg"].tags.join(", ")}
-      </CardSubtitle>
-    </CardBody>
-  </Card>
-);
+  return (
+    <Card className={`hover-card p-1 ${expanded ? "" : "mb-2"}`}>
+      <CardImg
+        top
+        width="100%"
+        src={`/images/${imgName}`}
+        alt=""
+        className={expanded ? "" : "display-small-img"}
+      />
+      <CardBody>
+        <CardTitle tag="h3">{description.title}</CardTitle>
+        {expanded ? (
+          <>
+            <CardText>
+              {description.description ?? "Description goes here"}
+            </CardText>
+            <hr />
+            <CardSubtitle className="text-muted">
+              Tags: {description.tags.join(", ")}
+            </CardSubtitle>
+            <hr />
+          </>
+        ) : (
+          ""
+        )}
+      </CardBody>
+    </Card>
+  );
+};
 
 function App() {
   const relevantImages = getRelevantImages("1.jpg");
@@ -67,17 +67,17 @@ function App() {
         lg="6"
         className="order-lg-1 d-flex flex-column justify-content-around"
       >
-        <LargeDisplay imgName="1.jpg" />
+        <Display imgName="1.jpg" expanded />
       </Col>
       <h4 className="mt-4 d-lg-none">Similar Images</h4>
       <Col lg="3" className="d-flex flex-column justify-content-around">
-        {relevantImages.slice(1, 4).map((id) => (
-          <SmallDisplay imgName={id as any} />
+        {relevantImages.slice(1, 4).map((fileName) => (
+          <Display imgName={fileName} />
         ))}
       </Col>
       <Col lg="3" className="order-2 d-flex flex-column justify-content-around">
-        {relevantImages.slice(5, 8).map((id) => (
-          <SmallDisplay imgName={id as any} />
+        {relevantImages.slice(5, 8).map((fileName) => (
+          <Display imgName={fileName} />
         ))}
       </Col>
     </div>
