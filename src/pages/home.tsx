ca69772d128cd5e433eaedfc595d49f934b5e5e1
Mike <mnightingale@tutanota.com>
2021-01-27 20:46:28 +0000

    Add home title

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,17 +3,21 @@ import { Display } from "../components/display";
 
 export const HomePage = () => {
   return (
-    <div className="container-lg d-flex mt-4">
-      <Col xs="12" md="6">
-        {[1, 3, 5, 7, 9].map((id) => (
-          <Display imgName={`${id}.jpg` as any} />
-        ))}
-      </Col>
-      <Col xs="12" md="6">
-        {[2, 4, 6, 8, 10].map((id) => (
-          <Display imgName={`${id}.jpg` as any} />
-        ))}
-      </Col>
-    </div>
+    <>
+      <h1 className="text-center mt-2">Images</h1>
+      <hr />
+      <div className="container-lg d-flex mt-4">
+        <Col xs="12" md="6">
+          {[1, 3, 5, 7, 9].map((id) => (
+            <Display imgName={`${id}.jpg` as any} />
+          ))}
+        </Col>
+        <Col xs="12" md="6">
+          {[2, 4, 6, 8, 10].map((id) => (
+            <Display imgName={`${id}.jpg` as any} />
+          ))}
+        </Col>
+      </div>
+    </>
   );
 };
